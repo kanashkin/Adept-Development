4cfdf58d599ca3ff38dbc30f0bcdfb035d9b69b8
Alexander <kanashkin_ad@mail.ru>
2024-05-12 11:39:14 +0300

    ready adaptive from 999 to 768

--- js/app.js
+++ js/app.js
@@ -19,14 +19,23 @@ function accordeon() {
     })
 
     items.forEach(function(item, i) {
-        item.addEventListener('mouseenter', function(e) {
-            item.classList.add('active')
-            item.style.height = activeHeights[i] + 'px'
-        })
-        item.addEventListener('mouseleave', function(e) {
-            item.classList.remove('active')
-            item.style.height = baseHeights[i] + 'px'
-        })
+        if (window.innerWidth >= 1000) {
+            item.addEventListener('mouseenter', function(e) {
+                item.classList.add('active')
+                item.style.height = activeHeights[i] + 'px'
+            })
+            item.addEventListener('mouseleave', function(e) {
+                item.classList.remove('active')
+                item.style.height = baseHeights[i] + 'px'
+            })
+        } else {
+            window.addEventListener('scroll', function() {
+                if (item.getBoundingClientRect().top <= window.innerHeight / 2) {
+                    item.classList.add('active')
+                    item.style.height = activeHeights[i] + 'px'
+                }
+            })
+        }
     })
 }
 
@@ -34,6 +43,8 @@ setTimeout(accordeon, 500)
 
 
 function listAnimation() {
+    const list = document.querySelector('.promo__list')
+
     const listItems = document.querySelectorAll('.promo__list-item');
     let index = 0;
 
@@ -45,7 +56,39 @@ function listAnimation() {
         }
     }
 
-    animateNextItem();
+    function checkList() {
+        if (list.getBoundingClientRect().top <= window.innerHeight) {
+            window.removeEventListener('scroll', checkList)
+            animateNextItem();
+        }
+    }
+
+    window.addEventListener('scroll', checkList)
 }
 
 setTimeout(listAnimation, 500)
+
+function moveAccordeonLink() {
+    const contents = document.querySelectorAll('.accordeon__item__content')
+    const arrows = document.querySelectorAll('.accordeon__item-link')
+
+    arrows.forEach(function(item, i) {
+        contents[i].append(item)
+    })
+}
+
+function moveCasesItems() {
+    const columns = document.querySelectorAll('.cases__column')
+    const currentItems = columns[1].querySelectorAll('.cases__item')
+
+    columns[0].append(currentItems[0])
+    columns[2].append(currentItems[1])
+    currentItems[1].style.order = -1
+
+    columns[1].remove()
+}
+
+if (window.innerWidth <= 999) {
+    moveAccordeonLink()
+    moveCasesItems()
+}
